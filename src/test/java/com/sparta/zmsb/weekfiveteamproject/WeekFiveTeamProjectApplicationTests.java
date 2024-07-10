a8472ff5523ca5450d502fc4845ef9d03a0ad04a
Tam <enquiretam@gmail.com>
2024-07-10 15:31:15 +0100

    first commit

---
+++ src/test/java/com/sparta/zmsb/weekfiveteamproject/WeekFiveTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.zmsb.weekfiveteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekFiveTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
