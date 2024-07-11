3574000078af68c9e6f19f72a482f00b76c1a43c
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-07-11 16:40:44 +0100

    Finalised update abstraction method

--- src/main/java/com/sparta/zmsb/weekfiveteamproject/updates/CountryLanguageUpdates.java
+++ src/main/java/com/sparta/zmsb/weekfiveteamproject/updates/CountryLanguageUpdates.java
@@ -1,9 +1,14 @@
 package com.sparta.zmsb.weekfiveteamproject.updates;
 
+import com.sparta.zmsb.weekfiveteamproject.entities.CountryEntity;
 import com.sparta.zmsb.weekfiveteamproject.entities.CountrylanguageEntity;
+import com.sparta.zmsb.weekfiveteamproject.entities.CountrylanguageEntityId;
 import com.sparta.zmsb.weekfiveteamproject.service.WorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 public class CountryLanguageUpdates {
@@ -11,8 +16,20 @@ public class CountryLanguageUpdates {
     @Autowired
     WorldService worldService;
 
-    public CountrylanguageEntity createEntity(Integer id, String value, String flag) {
+    public CountrylanguageEntity createEntity(CountrylanguageEntityId id, String value, String flag) {
+        List<CountryEntity> countries = worldService.allCountries();
+        CountrylanguageEntity entity = worldService.allLanguages().stream().filter(c -> c.getId().equals(id)).findFirst().get();
 
-        return new CountrylanguageEntity();
+        switch (flag.toLowerCase()) {
+            case "is official":
+                entity.setIsOfficial(value);
+                break;
+            case "percentage":
+                entity.setPercentage(BigDecimal.valueOf(Double.parseDouble(value)));
+                break;
+            default:
+                break;
+        }
+        return entity;
     }
 }
