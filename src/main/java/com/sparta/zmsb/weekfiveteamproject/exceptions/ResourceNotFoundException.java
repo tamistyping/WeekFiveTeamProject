e9c9912155cb2791f4c25893aed2989df147251c
Tam <enquiretam@gmail.com>
2024-07-18 08:26:35 +0100

    resource not found exception

---
+++ src/main/java/com/sparta/zmsb/weekfiveteamproject/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sparta.zmsb.weekfiveteamproject.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
