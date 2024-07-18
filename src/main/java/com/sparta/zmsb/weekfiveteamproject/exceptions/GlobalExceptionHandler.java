757f7a199809d081a8bc848be97fb92fd0c468b9
Tam <enquiretam@gmail.com>
2024-07-18 11:02:10 +0100

    invalid input exception

--- src/main/java/com/sparta/zmsb/weekfiveteamproject/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/zmsb/weekfiveteamproject/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.sparta.zmsb.weekfiveteamproject.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +13,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(Exception e, HttpServletRequest request) {
         return new ResponseEntity<>(new ErrorResponse("NOT_FOUND", e.getMessage(), request.getRequestURL().toString()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidInputException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleInvalidInputException(Exception e, HttpServletRequest request) {
+        return new ResponseEntity<>(new ErrorResponse("BAD_REQUEST", e.getMessage(), request.getRequestURL().toString()), HttpStatus.BAD_REQUEST);
     }
 
     private record ErrorResponse(Object errorDetails, String errorCode, String url){}
---
+++ src/main/java/com/sparta/zmsb/weekfiveteamproject/exceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.sparta.zmsb.weekfiveteamproject.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
