2d9cb5295de6a1d8562b09f7ca3f2c5f88dd8571
Tam <enquiretam@gmail.com>
2024-07-18 14:52:38 +0100

    updated exceptions

--- src/main/java/com/sparta/zmsb/weekfiveteamproject/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/zmsb/weekfiveteamproject/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sparta.zmsb.weekfiveteamproject.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,6 +20,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleInvalidInputException(Exception e, HttpServletRequest request) {
         return new ResponseEntity<>(new ErrorResponse("BAD_REQUEST", e.getMessage(), request.getRequestURL().toString()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ErrorResponse> handleDuplicateResourceException(Exception e, HttpServletRequest request) {
+        return new ResponseEntity<>(new ErrorResponse("CONFLICT", e.getMessage(), request.getRequestURL().toString()), HttpStatus.CONFLICT);
     }
 
     private record ErrorResponse(Object errorDetails, String errorCode, String url){}
