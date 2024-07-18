067bf85c68c3e2b196f76f8289e555d9954e58e3
Chris Rayner <crayner@spartaglobal.com>
2024-07-19 00:29:58 +0100

    minor change to try to make swagger UI work with auth key

--- src/main/java/com/sparta/zmsb/weekfiveteamproject/config/WebConfig.java
+++ src/main/java/com/sparta/zmsb/weekfiveteamproject/config/WebConfig.java
@@ -3,13 +3,10 @@ package com.sparta.zmsb.weekfiveteamproject.config;
 import com.sparta.zmsb.weekfiveteamproject.apiauth.ApiKeyInterceptor;
 import com.sparta.zmsb.weekfiveteamproject.logging.AppLogger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 @Configuration
