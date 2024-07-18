07f2d17302af238bcbb99d45f412ef7f3524dc38
Chris Rayner <crayner@spartaglobal.com>
2024-07-18 10:36:59 +0100

    finalised API Key

--- src/main/java/com/sparta/zmsb/weekfiveteamproject/apiauth/ApiKeyInterceptor.java
+++ src/main/java/com/sparta/zmsb/weekfiveteamproject/apiauth/ApiKeyInterceptor.java
@@ -1,14 +1,19 @@
 package com.sparta.zmsb.weekfiveteamproject.apiauth;
 
 import com.sparta.zmsb.weekfiveteamproject.config.ApiKeyConfig;
+import com.sparta.zmsb.weekfiveteamproject.logging.AppLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.logging.Logger;
+
 @Component
 public class ApiKeyInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = AppLogger.getLogger();
 
     @Autowired
     private ApiKeyConfig apiKeyConfig;
@@ -17,7 +22,9 @@ public class ApiKeyInterceptor implements HandlerInterceptor {
         this.apiKeyConfig = apiKeyConfig;
     }
 
-    public boolean prehandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+        logger.info("Entered preHandle method");
         String requestApiKey = request.getHeader("x-api-key");
         if(apiKeyConfig.getApiKey().equals(requestApiKey)) {
             return true;
